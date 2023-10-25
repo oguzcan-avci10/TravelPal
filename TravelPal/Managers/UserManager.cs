d033f8d3418adb930e65aca22becf7317a1dbf71
Oguzcan Avci <Oguzcan.avci95@gmail.com>
2023-10-25 10:57:39 +0200

    Initial commit

---
+++ TravelPal/Managers/UserManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Models;
+
+namespace TravelPal.Managers
+{
+    public static class UserManager
+    {
+        public static List<IUser> Users = new List<IUser>();
+        
+
+        
+    }
+}
---
+++ TravelPal/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPal.Models
+{
+    public class Admin : IUser
+    {
+        public string Username { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Password { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+    }
+}
