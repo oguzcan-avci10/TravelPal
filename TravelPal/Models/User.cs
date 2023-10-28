e92e6306dd002be8743c14ba0e0eaada48209639
Oguzcan Avci <Oguzcan.avci95@gmail.com>
2023-10-28 14:59:51 +0200

    Added travel window.

--- TravelPal/Models/User.cs
+++ TravelPal/Models/User.cs
@@ -10,6 +10,8 @@ namespace TravelPal.Models
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public List<Travel> Travels { get; set; }   
        
         public User(string username, string password)
         {
